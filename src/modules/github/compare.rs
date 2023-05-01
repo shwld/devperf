62c2e24162730e3cea0614b67dd8b43b23be35e8
shwld <shu.account@outlook.com>
2023-05-01 09:56:43 +0900

    🐞fix: baseとheadが一緒ならheadのcommitを参照

--- src/modules/github/compare.rs
+++ src/modules/github/compare.rs
@@ -2,6 +2,11 @@ pub async fn get_first_commit_committer_date(owner: &str, repo: &str, base: &str
     if base.is_empty() || head.is_empty() {
         return super::repo::get_created_at(owner, repo).await
     }
+    if base == head {
+        let commit = super::commit::get(owner, repo, base).await.expect("Could not get commit");
+        let date = commit.commit.author.and_then(|author| author.date).or_else(|| commit.commit.comitter.and_then(|comitter| comitter.date)).expect("commit date not found");
+        return Ok(date)
+    }
     let crab = super::client::create_github_client().await;
     let path = format!("https://api.github.com/repos/{owner}/{repo}/compare/{base}...{head}", owner = owner, repo = repo, base = base, head = head);
     let result = crab._get(path, None::<&()>).await?;
@@ -10,6 +15,9 @@ pub async fn get_first_commit_committer_date(owner: &str, repo: &str, base: &str
         return super::repo::get_created_at(owner, repo).await
     }
     let res = result.json::<serde_json::Value>().await.expect("Could not parse response");
+    // log::debug!("res: {:?}", res);
+    log::debug!("base: {:?}, head: {:?}", base, head);
+    log::debug!("commits: {:?}", res.get("commits").expect("Could not get commits"));
     let date = res.get("commits").expect("Could not get commits")[0]
         .get("commit").expect("Could not get commit")
         .get("committer").expect("Could not get committer")
