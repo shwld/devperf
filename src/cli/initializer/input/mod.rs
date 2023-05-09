0520666ab76f047917490c40db2ff63729a7107b
shwld <shu.account@outlook.com>
2023-05-09 22:09:27 +0900

    👷chore: implement cli create heroku release

--- src/cli/initializer/input/mod.rs
+++ src/cli/initializer/input/mod.rs
@@ -1,5 +1,7 @@
 pub mod developer_count;
 pub mod github_personal_token;
 pub mod github_owner_repo;
+pub mod heroku_api_token;
+pub mod heroku_app_name;
 pub mod project_name;
 pub mod working_days_per_week;
