4f3397b76e52f1abe635ddd659938ea4034e1fe2
shwld <shu.account@outlook.com>
2023-05-12 17:46:11 +0900

    🐞fix: appname input type

--- src/apps/cli/initializer/input/heroku_app_name.rs
+++ src/apps/cli/initializer/input/heroku_app_name.rs
@@ -1,13 +1,9 @@
-use inquire::{Password, PasswordDisplayMode};
+use inquire::Text;
 
 use crate::common_types::heroku_app_name::ValidatedHerokuAppName;
 
 pub fn input() -> ValidatedHerokuAppName {
-    let value = Password::new("Type a Heroku app name: ")
-        .with_display_mode(PasswordDisplayMode::Masked)
-        .without_confirmation()
-        .prompt()
-        .unwrap();
+    let value = Text::new("Type a Heroku app name: ").prompt().unwrap();
     let value = ValidatedHerokuAppName::new(Some(value));
 
     if let Ok(value) = value {
