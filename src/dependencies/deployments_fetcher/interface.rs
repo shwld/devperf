fbf848a6de968747be41e9676fee268568ba472a
shwld <shu.account@outlook.com>
2023-05-16 08:46:38 +0900

    👷chore: implement public interface

--- src/dependencies/deployments_fetcher/interface.rs
+++ src/dependencies/deployments_fetcher/interface.rs
@@ -3,27 +3,13 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
+// Input
 pub struct DeploymentsFetcherParams {
     pub since: Option<DateTime<Utc>>,
     pub until: Option<DateTime<Utc>>,
 }
 
-#[derive(Debug, Error)]
-pub enum DeploymentsFetcherError {
-    #[error("Create API client error")]
-    CreateAPIClientError(#[source] anyhow::Error),
-    #[error("Fetch deployments error")]
-    FetchError(#[source] anyhow::Error),
-    #[error("Get commit error")]
-    CommitIsNotFound(#[source] anyhow::Error),
-    #[error("Cannot get repository created at")]
-    GetRepositoryCreatedAtError(#[source] anyhow::Error),
-    #[error("Fetch deployments result is empty list")]
-    DeploymentsFetcherResultIsEmptyList(#[source] anyhow::Error),
-    #[error("Invalid response: {0}")]
-    InvalidResponse(String),
-}
-
+// Output
 #[derive(Debug, Clone)]
 pub struct CommitItem {
     pub sha: String,
@@ -59,6 +45,24 @@ pub struct DeploymentItem {
     pub deployed_at: DateTime<Utc>,
 }
 
+// Errors
+#[derive(Debug, Error)]
+pub enum DeploymentsFetcherError {
+    #[error("Create API client error")]
+    CreateAPIClientError(#[source] anyhow::Error),
+    #[error("Fetch deployments error")]
+    FetchError(#[source] anyhow::Error),
+    #[error("Get commit error")]
+    CommitIsNotFound(#[source] anyhow::Error),
+    #[error("Cannot get repository created at")]
+    GetRepositoryCreatedAtError(#[source] anyhow::Error),
+    #[error("Fetch deployments result is empty list")]
+    DeploymentsFetcherResultIsEmptyList(#[source] anyhow::Error),
+    #[error("Invalid response: {0}")]
+    InvalidResponse(String),
+}
+
+// Workflow
 #[async_trait]
 pub trait DeploymentsFetcher {
     async fn fetch(
