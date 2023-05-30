9c05812c3a6798ae675b040456acd1cc01d37abf
shwld <shu.account@outlook.com>
2023-05-30 13:02:00 +0900

    🐞fix: clippy

--- src/test/factories/deployment_log.rs
+++ src/test/factories/deployment_log.rs
@@ -1,27 +1,27 @@
-use chrono::Utc;
+// use chrono::Utc;
 
-use crate::{
-    dependencies::deployments_fetcher::interface::{
-        BaseCommitShaOrRepositoryInfo, CommitItem, DeploymentInfo, DeploymentLog,
-    },
-    shared::datetime_utc::parse,
-};
+// use crate::{
+//     dependencies::deployments_fetcher::interface::{
+//         BaseCommitShaOrRepositoryInfo, CommitItem, DeploymentInfo, DeploymentLog,
+//     },
+//     shared::datetime_utc::parse,
+// };
 
-pub fn build_deployment_log(deployed_at_str: &str) -> DeploymentLog {
-    let deployed_at = parse(deployed_at_str).expect("Could not parse deployed_at_str");
-    DeploymentLog {
-        info: DeploymentInfo::GithubDeployment {
-            id: "id".to_string(),
-        },
-        head_commit: CommitItem {
-            sha: "sha".to_string(),
-            message: "message".to_string(),
-            resource_path: "resource_path".to_string(),
-            committed_at: Utc::now(),
-            creator_login: "creator_login".to_string(),
-        },
-        base: BaseCommitShaOrRepositoryInfo::BaseCommitSha("base".to_string()),
-        creator_login: "creator_login".to_string(),
-        deployed_at,
-    }
-}
+// pub fn build_deployment_log(deployed_at_str: &str) -> DeploymentLog {
+//     let deployed_at = parse(deployed_at_str).expect("Could not parse deployed_at_str");
+//     DeploymentLog {
+//         info: DeploymentInfo::GithubDeployment {
+//             id: "id".to_string(),
+//         },
+//         head_commit: CommitItem {
+//             sha: "sha".to_string(),
+//             message: "message".to_string(),
+//             resource_path: "resource_path".to_string(),
+//             committed_at: Utc::now(),
+//             creator_login: "creator_login".to_string(),
+//         },
+//         base: BaseCommitShaOrRepositoryInfo::BaseCommitSha("base".to_string()),
+//         creator_login: "creator_login".to_string(),
+//         deployed_at,
+//     }
+// }
--- src/test/factories/repository_info.rs
+++ src/test/factories/repository_info.rs
@@ -1,6 +1,6 @@
-use crate::{metrics_retrieving::retrieve_four_keys::RepositoryInfo, shared::datetime_utc::parse};
+// use crate::{metrics_retrieving::retrieve_four_keys::RepositoryInfo, shared::datetime_utc::parse};
 
-pub fn build_repository_info(created_at_str: &str) -> RepositoryInfo {
-    let created_at = parse(created_at_str).expect("Could not parse created_at_str");
-    RepositoryInfo { created_at }
-}
+// pub fn build_repository_info(created_at_str: &str) -> RepositoryInfo {
+//     let created_at = parse(created_at_str).expect("Could not parse created_at_str");
+//     RepositoryInfo { created_at }
+// }
