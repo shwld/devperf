61f3936768ae9e2c861e68850cfaaf031017b2ea
shwld <shu.account@outlook.com>
2023-05-30 12:40:42 +0900

    🚨test: medium performance

--- src/metrics_retrieving/retrieve_four_keys_public_tests.rs
+++ src/metrics_retrieving/retrieve_four_keys_public_tests.rs
@@ -112,8 +112,7 @@ mod tests {
                     //   0.8711111111 / 2 = 0.4355555555
                     //
                     // performance
-                    //   2.0(weekly_deploys_median) > (3 * (2.5 / 5)) = 1.5 -> High
-                    //   2.0(weekly_deploys_median) > (3 * (2.5 / 5)) = 1.5 -> Daily
+                    //   2.0(weekly_deploys_median) > (3 * (2.5 / 5)) = 1.5 -> Daily, High
                 ],
             };
             let first_commit_getter = FirstCommitGetterWithMock {
@@ -142,6 +141,122 @@ mod tests {
                         assert_eq!(frequency.deploys_per_a_day_per_a_developer, 0.435_555_58);
                         assert_eq!(label, DeploymentFrequencyLabel::Daily);
                         assert_eq!(performance, DeploymentFrequencyPerformanceSurvey2022::High);
+                    }
+                }
+            }
+        }
+
+        #[tokio::test]
+        async fn deployment_performance_is_medium() {
+            let context = RetrieveFourKeysExecutionContext {
+                timeframe: DateTimeRange::new(
+                    parse("2023-01-01 00:00:00").expect("Could not parse since"),
+                    parse("2023-03-31 00:00:00").expect("Could not parse since"),
+                )
+                .expect("Could not create timeframe"),
+                project: RetrieveFourKeysExecutionContextProject {
+                    name: "project".to_string(),
+                    developer_count: 2,
+                    working_days_per_week: 2.5,
+                },
+            };
+            let deployments_fetcher = DeploymentsFetcherWithMock {
+                deployment_logs: vec![
+                    build_deployment_log("2023-04-01 10:00:00"),
+                    build_deployment_log("2023-03-29 10:00:00"), // March    5th week
+                    build_deployment_log("2023-03-28 17:30:00"), // March    5th week
+                    build_deployment_log("2023-03-22 10:00:00"), // March    4th week
+                    build_deployment_log("2023-03-14 10:00:00"), // March    3th week
+                    build_deployment_log("2023-02-28 10:00:00"), // February 5th week
+                    build_deployment_log("2023-02-22 10:00:00"), // February 4th week
+                    build_deployment_log("2023-02-08 10:00:00"), // February 2nd week
+                    build_deployment_log("2023-01-31 10:00:00"), // January  5th week
+                    build_deployment_log("2023-01-25 10:00:00"), // January  4th week
+                    build_deployment_log("2023-01-10 10:00:00"), // January  2nd week
+                    build_deployment_log("2023-01-02 10:00:00"), // January  1st week
+                    build_deployment_log("2022-12-31 10:00:00"),
+                    // total deploys    = 11
+                    // all days         = 90
+                    //
+                    // weekly_deploys
+                    //   January  1st week: 1
+                    //   January  2nd week: 1
+                    //   January  3th week: 0
+                    //   January  4th week: 1
+                    //   January  5th week: 1
+                    //   February 2nd week: 1
+                    //   February 3th week: 0
+                    //   February 4th week: 1
+                    //   February 5th week: 1
+                    //   March    2nd week: 0
+                    //   March    3th week: 1
+                    //   March    4th week: 1
+                    //   March    5th week: 1
+                    //   [0,0,0,1,1,1,(1),1,1,1,1,1,1]
+                    //   -> Median:         1.0
+                    //
+                    // deployed_weeks
+                    //   January 1st week:  true(1)
+                    //   January 2nd week:  true(1)
+                    //   January 3th week:  false(0)
+                    //   January 4th week:  true(1)
+                    //   January 5th week:  true(1)
+                    //   February 2nd week: true(1)
+                    //   February 3th week: false(0)
+                    //   February 4th week: true(1)
+                    //   February 5th week: true(1)
+                    //   March 2nd week:    false(0)
+                    //   March 3th week:    true(1)
+                    //   March 4th week:    true(1)
+                    //   March 5th week:    true(1)
+                    //   [0,0,0,1,1,1,(1),1,1,1,1,1,1]
+                    //   -> Median:         1.0
+                    //
+                    // deployed_months
+                    //   January:           true(1)
+                    //   February:          true(1)
+                    //   March:             true(1)
+                    //   -> Median:         1.0
+                    //
+                    // per day
+                    //   11days / (90days * (2.5 / 7)) = 0.3422222222
+                    // per day per a developer
+                    //   0.3422222222 / 2 = 0.1711111111
+                    //
+                    // performance
+                    //   1.0(weekly_deploys_median) > (3 * (2.5 / 5)) = 1.5 -> Not enough
+                    //   1.0(deployed_weeks) >= 1.0 -> Medium, Weekly
+                ],
+            };
+            let first_commit_getter = FirstCommitGetterWithMock {
+                committed_at_str: "2023-01-02 10:00:00".to_string(),
+            };
+            let workflow = RetrieveFourKeysWorkflow {
+                deployments_fetcher,
+                first_commit_getter,
+            };
+            let result = workflow.retrieve_four_keys(context).await;
+            assert!(result.is_ok());
+
+            for item in result.unwrap() {
+                match item {
+                    RetrieveFourKeysEvent::RetrieveFourKeys(result) => {
+                        let all_days = result.deployments.len();
+                        let frequency = result.performance.deployment_frequency.value;
+                        let label = result.performance.deployment_frequency.label;
+                        let performance = result.performance.deployment_frequency.performance;
+                        assert_eq!(all_days, 90);
+                        assert_eq!(frequency.total_deployments, 11);
+                        assert_eq!(frequency.weekly_deployment_count_median, 1.0);
+                        assert_eq!(frequency.month_deployed_median, 1.0);
+                        assert_eq!(frequency.week_deployed_median, 1.0);
+                        assert_eq!(frequency.deployment_frequency_per_day, 0.342_222_2);
+                        assert_eq!(frequency.deploys_per_a_day_per_a_developer, 0.171_111_1);
+                        assert_eq!(label, DeploymentFrequencyLabel::Weekly);
+                        assert_eq!(
+                            performance,
+                            DeploymentFrequencyPerformanceSurvey2022::Medium
+                        );
                     }
                 }
             }
